571e64c5e8c6a2dd922531a18ced0e1b9c654c42
Marcos <marcosallanm1to@gmail.com>
2024-06-23 12:22:02 -0300

    Order, Instant, ISO 8601 - PART 2

---
+++ src/main/java/com/marcosallan/course/services/OrderService.java
@@ -0,0 +1,26 @@
+package com.marcosallan.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.marcosallan.course.entities.Order;
+import com.marcosallan.course.repositories.OrderRepository;
+
+@Service
+public class OrderService {
+	
+	@Autowired
+	private OrderRepository orderRepository;
+	
+	public List<Order> findAll() {
+		return orderRepository.findAll();
+	}
+	
+	public Order findById(Long id) {
+		Optional<Order> obj = orderRepository.findById(id);
+		return obj.get();
+	}
+}
