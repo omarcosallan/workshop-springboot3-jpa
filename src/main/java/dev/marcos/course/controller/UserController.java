43126b3def3e59eddb19a089a4135d769b799051
Marcos Allan <marcosallanm1to@gmail.com>
2025-12-06 15:51:41 -0300

    Service layer, component registration

--- src/main/java/dev/marcos/course/controller/UserController.java
+++ src/main/java/dev/marcos/course/controller/UserController.java
@@ -1,18 +1,34 @@
 package dev.marcos.course.controller;
 
 import dev.marcos.course.entity.User;
+import dev.marcos.course.service.UserService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
 
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
     @GetMapping
-    public ResponseEntity<User> findAll() {
-        User u = new User(1L, "Maria", "maria@gmail.com", "99999-9999", "12345");
-        return ResponseEntity.ok().body(u);
+    public ResponseEntity<List<User>> findAll() {
+        List<User> users = userService.findAll();
+        return ResponseEntity.ok().body(users);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id) {
+        User user = userService.findById(id);
+        return ResponseEntity.ok().body(user);
     }
 }
---
+++ src/main/java/dev/marcos/course/service/UserService.java
@@ -0,0 +1,27 @@
+package dev.marcos.course.service;
+
+import dev.marcos.course.entity.User;
+import dev.marcos.course.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    public User findById(Long id) {
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("User not found with id " + id));
+        return user;
+    }
+}
